6b6f56545f348734d5f3afed2ec36f428d015350
dheerajrathore <rathored070@gmail.com>
2025-09-30 22:00:07 +0530

    Operators in python

---
+++ python_genai/python_tutor/operators/logical_oper.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+c = 30
+print(a > b and b < c)  # True
+a = 'U'
+print(ord(a))  # 85
+a = 'Umbrella'
+# TypeError: ord() expected a character, but string of length 8 found
+# Uncommenting the next line will raise a TypeError
+# print(ord(a))
+
