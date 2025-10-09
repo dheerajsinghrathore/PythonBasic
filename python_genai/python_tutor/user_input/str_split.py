b305c61c2f78a93d6e26a46c785efd2248755285
dheerajrathore <rathored070@gmail.com>
2025-10-09 22:20:33 +0530

    Eval function, command line arguments

---
+++ python_genai/python_tutor/user_input/str_split.py
@@ -0,0 +1,3 @@
+roll, name = input("Enter roll number and name separated by space: ").split()
+print("Roll Number:", roll)
+print("Name:", name)#   Print roll number and name based on user input separated by space.
